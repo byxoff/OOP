7cd05193201e38ddec0277240b5f6753fbc3cf2f
Byxoff <i.byxoff@yandex.ru>
2022-02-03 17:06:31 +0500

    another 2 buttons

--- Shared/Buttons.swift
+++ Shared/Buttons.swift
@@ -9,22 +9,53 @@ import SwiftUI
 
 struct Buttons: View {
     var body: some View {
-        Button(action: {print("сработало")}, label: {
+        ScrollView {
             HStack {
-                Text("записаться")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.gray)
-            .padding(.horizontal, 70.0)
-            .padding(.vertical, 5.0)
-            }.background(Color("yellowButtons")).cornerRadius(10.0)
-                
-        })
+                Spacer()
+                VStack {
+                    Spacer().frame(height: 300.0)
+                    YellowButton()
+                    ServicesButton()
+                    Spacer()
+                }
+                Spacer()
+            }
+        }.ignoresSafeArea(.all).background(Color.black)
     }
 }
 
 struct Buttons_Previews: PreviewProvider {
     static var previews: some View {
         Buttons()
+    }
+}
+
+struct YellowButton: View {
+    var body: some View {
+        Button(action: {print("сработало")}, label: {
+            HStack {
+                Text("записаться")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.black)
+                    .padding(.horizontal, 80.0)
+                    .padding(.vertical, 5.0)
+            }.background(Color("yellowButtons")).cornerRadius(10.0)
+            
+        })
+    }
+}
+
+struct ServicesButton: View {
+    var body: some View {
+        Button(action: {print("сработало")}){
+                Text("услуги")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.white)
+                    .padding(.horizontal, 10.0)
+                    .padding(.vertical, 5.0)
+        }.frame(width: 120, height: 40).overlay(RoundedRectangle(cornerRadius: 10)
+            .stroke((Color.white), lineWidth: 2))
     }
 }
